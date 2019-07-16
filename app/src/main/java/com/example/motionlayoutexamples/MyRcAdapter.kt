20e98d0e66d0f0f42c86a586b6c9d6f3ae5a4b8a
supto <supto09apee@gmail.com>
2019-07-16 22:20:51 +0600

    Parallax effect implemented

---
+++ app/src/main/java/com/example/motionlayoutexamples/MyRcAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.motionlayoutexamples
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_recycler_view.view.*
+
+class MyRcAdapter(private val context: Context) : ListAdapter<String, MyRcViewHolder>(MyDiffCallback()) {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyRcViewHolder {
+        val view = LayoutInflater.from(this.context).inflate(R.layout.item_recycler_view, parent, false)
+        return MyRcViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyRcViewHolder, position: Int) {
+        holder.textView.text = getItem(position)
+    }
+
+
+}
+
+class MyRcViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val textView = itemView.textView
+}
+
+
+class MyDiffCallback : DiffUtil.ItemCallback<String>() {
+
+    override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem == newItem
+    }
+}
---
+++ app/src/main/java/com/example/motionlayoutexamples/ParallaxFragment.kt
@@ -0,0 +1,50 @@
+package com.example.motionlayoutexamples
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_parallax.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ParallaxFragment : Fragment() {
+
+    private lateinit var adapter: MyRcAdapter
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_parallax, container, false)
+    }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val languageList = listOf(
+            "Godzilla: King of the Monsters",
+            "Toy Story 4",
+            "Captain Marvel",
+            "Aladdin",
+            "John Wick: Chapter 3 – Parabellum",
+            "Shazam!",
+            "Annabelle Comes Home",
+            "Crawl",
+            "Pet Sematary",
+            "Hellboy",
+            "Alita: Battle Angel",
+            "Ad Astra",
+            "Joker",
+            "Avengers: Endgame"
+        )
+
+        adapter = MyRcAdapter(requireContext())
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = adapter
+        adapter.submitList(languageList)
+    }
+
+}
