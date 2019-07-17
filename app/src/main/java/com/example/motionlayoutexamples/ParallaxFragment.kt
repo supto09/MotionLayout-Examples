01e746e7794161d789e8d56a9776a8e8297b496f
supto <ashik@cloudly.io>
2019-07-17 13:14:12 +0600

    SSc uploaded

--- app/src/main/java/com/example/motionlayoutexamples/ParallaxFragment.kt
+++ app/src/main/java/com/example/motionlayoutexamples/ParallaxFragment.kt
@@ -15,7 +15,11 @@ class ParallaxFragment : Fragment() {
 
     private lateinit var adapter: MyRcAdapter
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_parallax, container, false)
     }
